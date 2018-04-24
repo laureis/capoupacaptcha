fa14e671aacd42a818524d37f8cca33b5157e7da
Laure <issalaure@yahoo.fr>
2018-04-24 19:11:34 +0200

    turning theme into in interface step 1é

--- src/fr/upem/captcha/Theme.java
+++ src/fr/upem/captcha/Theme.java
@@ -1,9 +1,17 @@
 package fr.upem.captcha;
 
+import fr.upem.captcha.graphics.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Theme {
 
+    private int level;
     private String name;
     private String url;
+    private ArrayList<CaptchaImage> images = new ArrayList<CaptchaImage>();
 
     public String getName() {
         return this.name;
@@ -13,6 +21,10 @@ public class Theme {
         return this.url;
     }
     
+    public ArrayList<CaptchaImage> getImages() {
+       return this.images;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -21,8 +33,43 @@ public class Theme {
         this.url = url;
     }
 
+    public void setImages(ArrayList<CaptchaImage> images) {
+        this.images = images;
+    }
+
+    public void setImages() {
+
+        File[] listImages = new File("src"+File.separator+"fr"+File.separator+"upem"+File.separator+"captcha"+File.separator+"images"+File.separator+this.name).listFiles();        
+        for (int i = 0; i < listImages.length; i++) {
+            this.images.add(new CaptchaImage(listImages[i].getName(), listImages[i].getAbsolutePath(), null));
+        }
+    }
+
     public Theme (String name, String url) {
         this.name = name;
         this.url = url;
+        setImages();
     }
+
+    public ArrayList<CaptchaImage> getImages(int number) {
+        Collections.shuffle(this.images);
+        ArrayList<CaptchaImage> nImages = new ArrayList<CaptchaImage>();
+        for (int i = 0; i < number; i++) {
+            nImages.add(this.images.get(i));
+        }
+        return nImages;
+    }
+
+    public boolean isCorrect(CaptchaImage image) {
+
+        for (CaptchaImage i : this.images) {
+            if (image == i) return true;
+        }
+        return false;
+    }
+
+    public void upgradeLevel() {
+
+    }
+
 }
